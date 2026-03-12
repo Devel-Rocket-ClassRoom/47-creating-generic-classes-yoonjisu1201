15b8debef86c895347375c24dd97ea17270a03ce
yoonjisu1201 <yoonjisu1201@gmail.com>
2026-03-12 18:02:48 +0900

    Update Program.cs

--- GenericConverter/Program.cs
+++ GenericConverter/Program.cs
@@ -1,4 +1,10 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+class Converter<TInput, TOutput> // 타입이 2개 
+{
+    public Convert(TOutput) // 변환 원하는거 받기
+    {
+
+    }
+}
